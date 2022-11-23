6c414d167ac6dd8f32935cbb504a51605dddb991
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2022-11-23 14:21:52 +0900

    [level 1] Title: 약수의 합, Time: 3.02 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        return (int) IntStream.rangeClosed(1, n).filter(v -> n % v == 0).map(i->i).sum();
+    }
+}
