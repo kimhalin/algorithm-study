1505de3689e612b20c0e81e0af5bc65eee67de81
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2022-11-23 13:41:02 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.88 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0){
+                list.add(i);
+            }
+        }
+        return list.stream().mapToInt(i->i).toArray();
+        
+    }
+}
