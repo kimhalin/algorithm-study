6f4e249e704905f44529d852e8ef4af1eb4a9a74
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2023-04-04 14:18:20 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,17 @@
+N = int(input())  # input
+ 
+# dp = [0 for i in range(N)]
+# dp = [0] * N
+dp = [0] * (1000 + 1)
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 1
+ 
+for n in range(4, N+1):
+    dp[n] = min(dp[n-1], dp[n-3]) + 1
+ 
+if dp[N] % 2 == 1:
+    print('SK')
+else:
+    print('CY')
