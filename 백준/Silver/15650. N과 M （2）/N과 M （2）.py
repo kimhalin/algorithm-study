a0ebc0fb4e1d9257bb76a7be014093c11eabe52c
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2023-08-09 17:06:27 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+arr = []
+
+def recurr(count):
+    if count == M:
+        print(*arr)
+        return
+    for i in range(1, N + 1):
+        if i in arr or (arr and i <= max(arr)):
+            continue
+        arr.append(i)
+        recurr(count+1)
+        arr.pop()
+
+recurr(0)
