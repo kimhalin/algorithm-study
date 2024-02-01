0ff4cc32e771cc5938c6e521ec4c6aae78433b6f
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-02-02 02:09:41 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,22 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(String name) {
         int answer = 0;
         int length = name.length();
-
-        int index; 
-        int move = length - 1; 
-
-        for(int i = 0; i < name.length(); i++){
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-
+        
+        int index;
+        int move = length - 1;
+        
+        for (int i = 0; i < name.length(); i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z', - name.charAt(i) + 1);
+            
             index = i + 1;
-            while(index < length && name.charAt(index) == 'A'){
-                index++;
-            }
+            
+            while (index < length && name.charAt(index) == 'A') index++;
             
             move = Math.min(move, i * 2 + length - index);
-            move = Math.min(move, (length - index) * 2 + i);
+            move = Math.min(move, (name.length() - index) * 2 + i);
+            
         }
+        
         return answer + move;
     }
 }
