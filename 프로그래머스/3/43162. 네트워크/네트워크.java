cbcdb41012cc6b3266c56a47ddb2b84d76f26cdd
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-02-03 18:48:02 +0900

    [level 3] Title: 네트워크, Time: 0.11 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,25 @@
 class Solution {
-    boolean[] visit;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visit = new boolean[n];
+        boolean[] visited = new boolean[computers.length];
         
-        for (int i =0; i < n; i++) {
-            if (visit[i] == false) {
+        for(int i = 0; i < computers.length; i++){
+            if(visited[i] == false){
                 answer++;
-                dfs(computers, n, i);
-            }
+                dfs(i, visited, computers);
+            }            
         }
-        return answer;
         
+        return answer;
     }
     
-    public void dfs(int computers[][], int n, int idx) {
-        visit[idx] = true;
+    public void dfs(int node, boolean[] visited, int[][] computers){
+        visited[node] = true;
         
-        for (int i = 0; i < n; i++) {
-            if (visit[i] == false && computers[idx][i] == 1)
-                dfs(computers, n, i);
+        for(int i = 0; i < computers.length; i++){
+            if(visited[i] == false && computers[node][i] == 1){
+                dfs(i, visited, computers);
+            }
         }
     }
 }
